c1ab084867492199070c7cdc74211163d218ee10
jiang-xia <963798512@qq.com>
2022-07-26 12:35:40 +0800

    测试：git reset --hard

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,8 +13,8 @@ if (mode === 'production') {
   // baseUrl = 'http://42.192.145.236:5000'
   baseUrl = 'https://jiang-xia.top/x-api/blog-server'
 } else {
-  // baseUrl = 'http://localhost:5000'
+  baseUrl = 'http://localhost:5000'
   // baseUrl = 'http://42.192.145.236:5000'
-  baseUrl = 'https://jiang-xia.top/x-api/blog-server'
+  // baseUrl = 'https://jiang-xia.top/x-api/blog-server'
 }
 export default config
